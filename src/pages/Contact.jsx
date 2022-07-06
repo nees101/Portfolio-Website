952d3c42eec5827eacd123b2e3a6984855397a8d
Neethu Sasidharan Nair <86210328+nees101@users.noreply.github.com>
2022-07-05 21:09:58 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,9 +11,7 @@ function Contact(){
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        // this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
+     
         emailjs.sendForm('service_to6quka', "template_ooy7e5r" , "#contact-form", "0vu1Wx9yCMHAAXM5m")
             .then(function() {
                 console.log('SUCCESS!');
