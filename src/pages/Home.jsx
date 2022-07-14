1c037a046d1032dbd17ee8573204acac6e6d58fb
Neethu Sasidharan Nair <86210328+nees101@users.noreply.github.com>
2022-07-14 12:39:40 -0500

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ function Home(){
                <h2 className="text-center py-5 title"style={{color:"#F0A500"}}>Who Am I ?</h2>
           
                <div className="row mb-5 aboutrow"> 
-                  <div className="col-12 col-md-6 col-lg-6" >
+                  <div className="col-12  col-lg-6" >
                   <div className="pic-box m-auto">
                     <div className="frame"> </div>
                     <div className="slide">
@@ -26,12 +26,14 @@ function Home(){
                      </div>
                      </div> 
                   </div>
-                  <div className ="col-12  col-md-6 col-lg-6 px-5 aboutme">
+                  <div className ="col-12  col-lg-6 px-5 aboutme">
                   
                      <p > I’m an aspiring front-end developer looking for a new role in an exciting company. I have been self learning how to create websites focusing on writing accessible HTML, using modern CSS practices and writing clean JavaScript. 
-                     I’m based in Austin, TX, but I’m open to working remotely as well.
-                     </p>
-                     <p> When I’m not coding, you’ll find me outdoors. I’d love you to check out my work.</p>
+                     My coding journey began last year October 2021 and since then I have been coding almost everyday. As part of my learning I have completed courses on Udemy , freeCodecamp etc. and undertaken couple of challenges and projects that has helped me in improving my skills tremendously.
+                     </p> 
+                     <p> Apart from coding I love Math and have been educating kids of all ages over the past couple of years. I love nature and enjoy travelling whenever possible.</p>
+                      <p> I’m originally from India but currently based in Austin, TX. Also, I’m open to working remotely as well. I’d love you to check out my work.</p>
+                      <br/>
                       <a href="/portfolio" type="button" className="btn btn-outline-light  ">GO TO PORTFOLIO  <i className="fa-solid fa-angles-right"></i></a>
                   </div>
                 </div>
@@ -41,53 +43,53 @@ function Home(){
     
                       <p className="text-center text-muted mb-5" >The skills, tools and technologies  I’ve been working with recently..</p>
                       <div className="text-center" >
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                           <i className="devicon devicon-html5-plain-wordmark" aria-hidden="true"></i>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                           <i className=" devicon devicon-css3-plain-wordmark"></i>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                           <i className="devicon devicon-javascript-plain"></i>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                        <i className="fa-solid fa-mobile-screen fa-3x"></i>
                           <p>Responsive Web Design</p>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                          <i className="devicon devicon-jquery-plain-wordmark"></i>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                            <i className="devicon devicon-react-original-wordmark"></i>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                          <i className="devicon devicon-bootstrap-plain-wordmark"></i>
                
                         </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                          <i className="fas fa-3x fa-cogs" aria-hidden="true"></i>
                           <p>API</p>
                        </div>
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                           <i className="devicon devicon-git-plain-wordmark"></i>
                   
                         </div>
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                           <i className="devicon devicon-github-original-wordmark"></i>
                 
                         </div>
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                           <i className="fa-solid fa-terminal fa-2x"></i>
                          <p>Terminal</p>
                        </div>
-                       <div className="box-icon">
+                       <div className="box-icon col-lg-2 col-3">
                          <i className="devicon devicon-vscode-plain-wordmark"></i>
                         </div>
                   
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                            <i className=" devicon devicon-atom-original-wordmark " style={{fontSize:100}}></i>
                         </div>
-                        <div className="box-icon">
+                        <div className="box-icon col-lg-2 col-3">
                         <i className="devicon-chrome-plain"style={{fontSize:50}}></i>
                           <p>Chrome Dev Tools</p>
                        </div>
